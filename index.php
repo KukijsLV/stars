d7b076293cd814efea148fa8af0b9388f1e9561d
Skolnieks <skolnieks.pjvt@pjvt.local>
2024-11-26 11:22:07 +0200

    izmaina

--- index.php
+++ index.php
@@ -10,13 +10,34 @@ $results = $conn->query("SELECT * FROM users");
 $data = $results->fetch_assoc();
 
 var_dump($data);
+echo "</br>";
+
+
+//izveido lai saglaba database php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $sql = "INSERT INTO users (username, pasword)
+         VALUES ('$username', '$password');";
+
+         $conn->query($sql);
+}
 
 //ielikt datus
 
-echo "<form>";
 
-echo "<input name='username'";
-echo "<input name='password' type= 'password' />";
+echo "<form method='POST'>";
+
+echo "<label>Lietotajvārds: <input name='username' /></label>"; 
+echo "</br>";
+echo "<label>Parole: <input name='password' type='password' /></label>";
+echo "</br>";
 echo "<input type='submit' value='saglabat' />";
 
 echo "</form>";
+
+
+//$accoc_masivs = ["vecums" => 6, "videjais_atzime" => 8, "ids" => 10];
+
+
---
+++ script.js
@@ -0,0 +1,10 @@
+let a = 0;
+
+function handelClick(){
+    a = a + 1;
+
+    document.getElementById("count"). textContent = a;
+    document.getElementById("input").value = a;
+}
+
+//document.getElementById("poga")
